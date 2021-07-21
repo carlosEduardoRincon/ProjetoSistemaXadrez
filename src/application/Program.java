1383fde3c5036cee51453f79d1dc87d640926a82
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2021-07-21 20:25:09 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+
+package application;
+
+/**
+ *
+ * @author Carlos Eduardo
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}

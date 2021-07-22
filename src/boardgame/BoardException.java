9d745729b3f96e1d097b19ff5acda81c7ab4028b
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2021-07-22 13:44:59 -0300

    Programação defensiva e tratamento de exceção

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+
+package boardgame;
+
+/**
+ *
+ * @author Carlos Eduardo
+ */
+public class BoardException extends RuntimeException {
+ 
+    public BoardException(String msg){
+        super(msg);
+    }
+}

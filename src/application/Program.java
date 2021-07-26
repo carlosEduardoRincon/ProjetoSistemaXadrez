27e8452ea7b0dc9e7889c0f769b741aba0afaad4
Carlos Eduardo Rincon Junior <85880891+carlosEduardoRincon@users.noreply.github.com>
2021-07-26 16:42:44 -0300

    Tratamento de exceções, movimento de peças

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -13,8 +16,34 @@ public class Program {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
         ChessMatch c1 = new ChessMatch();
-        UI.printBoard(c1.getPieces());
+        
+        while(true){
+            try {
+                UI.clearSreeen();
+                UI.printBoard(c1.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = c1.performChessMove(source, target);
+            }
+            catch (ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            
+        }
     }
     
 }
---
+++ src/chess/ChessException.java
@@ -0,0 +1,15 @@
+
+package chess;
+
+import boardgame.BoardException;
+
+/**
+ *
+ * @author Carlos Eduardo
+ */
+public class ChessException extends BoardException{
+    
+    public ChessException(String msg){
+        super(msg);
+    }
+}
